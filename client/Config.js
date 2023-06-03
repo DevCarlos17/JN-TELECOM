95cead250e97b4f1a90ce2d44b480eee743166a9
Carlos <carlosmba96@gmail.com>
2023-06-03 11:10:52 -0400

    Edited API

--- client/Config.js
+++ client/Config.js
@@ -1,3 +1,3 @@
 //export const API = "https://jn-telecom-service.onrender.com"
-export const API = "http://localhost:4000"
-//export const API = "http://localhost:4000" 
+//export const API = "http://localhost:4000"
+export const API = "https://jn-telecom.onrender.com" 
