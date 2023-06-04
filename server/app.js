3a8afbdba6153f5b33685324b1f94a9a6ca6e8db
Carlos <carlosmba96@gmail.com>
2023-06-04 09:24:54 -0400

    add corsOptions

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ import corsOptions from "./config/corsOptions.js";
 
 const app = express();
 
+
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', 'https://jn-telecom.onrender.com/');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
