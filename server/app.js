65955344b47017f2ddeba36194ce99bde08040f6
Carlos <carlosmba96@gmail.com>
2023-06-06 11:25:14 -0400

    update

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://jn-telecom.onrender.com/');
+  res.header('Access-Control-Allow-Origin', 'https://jn-telecom.onrender.com');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
