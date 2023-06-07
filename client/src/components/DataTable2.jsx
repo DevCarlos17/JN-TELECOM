fab5ba305e83bebc785e25d82f3cfa91e4b0a9de
Carlos <carlosmba96@gmail.com>
2023-06-07 11:11:51 -0400

    edited rowPerPageOptions

--- client/src/components/DataTable2.jsx
+++ client/src/components/DataTable2.jsx
@@ -499,7 +499,7 @@ export default function DataTableSales() {
         value={dataTable}
         paginator
         rows={rowPerPage}
-        rowsPerPageOptions={[5, 10, 15, 20, 25, 50]}
+        rowsPerPageOptions={[5, 10, 15, 20, 25, 50, 200, 400]}
         totalRecords={dataTable.length}
         style={{ fontSize: "14px" }}
         showGridlines
