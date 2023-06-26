4568cae23cab12b0823f8850721236dd0f0f5764
Carlos <carlosmba96@gmail.com>
2023-06-26 16:11:28 -0400

    added context VerticalGrowth

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,7 @@ import FinancialRecordsProvider from "./context/FinancialRecordsContext.jsx";
 import { SalesProvider } from "./context/salesContext.jsx";
 import { UserProvider } from "./context/userContext.jsx";
 import ContactProvider from "./context/contactContext.jsx";
+import VerticalGrowthProvider from "./context/verticalGrowthContext.jsx";
 import "primereact/resources/themes/saga-blue/theme.css"; // Importa el tema de PrimeReact que desees usar
 import "primereact/resources/primereact.min.css"; // Importa los estilos base de PrimeReact
 import "primeicons/primeicons.css"; // Importa los estilos de PrimeIcons
@@ -16,7 +17,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <SalesProvider>
         <ContactProvider>
           <FinancialRecordsProvider>
-            <App />
+            <VerticalGrowthProvider>
+              <App />
+            </VerticalGrowthProvider>
           </FinancialRecordsProvider>
         </ContactProvider>
       </SalesProvider>
