2595f9ec5b25ab4c29faa93bbccd5ae5129a2ad1
Carlos <carlosmba96@gmail.com>
2023-07-01 14:46:45 -0400

    added average in GraphicDataCard

--- client/src/components/GraphicsDataCard.jsx
+++ client/src/components/GraphicsDataCard.jsx
@@ -5,16 +5,19 @@ const GraphicsDataCard = ({ data }) => {
 
   const totalSales = sales.reduce((acc, curr) => (acc += curr?.value), 0);
   const totalPaid = sales.filter((sale) => sale.name === "PAGADA")[0]?.value;
+  const average = ((totalPaid / totalSales) * 100).toFixed(2);
+  const formatedAverage = isNaN(average) ? 0 : average;
 
   return (
-    <div className="flex flex-col gap-2 p-2 mb-2 bg-secondary-100 border-double shadow-slate-300 rounded-xl shadow-sm  w-[9vw]">
+    <div className="flex flex-col gap-2 p-2 mb-2 bg-secondary-100 border-double shadow-slate-300 rounded-xl shadow-sm  w-[10vw]">
       <span className="font-bold text-white">{`INSTALADAS: ${
         totalPaid ? totalPaid : 0
       }`}</span>
-      <hr className="border-gray-500/30" />
       <span className="font-bold text-white">{`INGRESADAS: ${
         totalSales ? totalSales : 0
-      }`}</span>{" "}
+      }`}</span>
+      <hr className="border-gray-500/30" />
+      <span className="font-bold text-white">{`% INSTALADAS : ${formatedAverage}`}</span>
     </div>
   );
 };
