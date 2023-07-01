eb6ec61c714c543a3de382fb418183dfa71f1de0
Carlos <carlosmba96@gmail.com>
2023-07-01 13:45:19 -0400

    edited GraphicsDataCard

--- client/src/components/GraphicsDataCard.jsx
+++ client/src/components/GraphicsDataCard.jsx
@@ -4,10 +4,13 @@ const CardDataGraph = ({ data }) => {
   const { data: sales } = data?.series[0];
 
   const totalSales = sales.reduce((acc, curr) => (acc += curr.value), 0);
+  const totalPaid = sales.filter((sale) => sale.name === "PAGADA")[0].value;
 
   return (
-    <div className="flex flex-col gap-2 p-2 mb-2 bg-secondary-100 border-double shadow-slate-300 rounded-xl shadow-sm w-[11vw]">
-      <span className="font-bold text-white">{`TOTAL INGRESADAS: ${totalSales}`}</span>
+    <div className="flex flex-col gap-2 p-2 mb-2 bg-secondary-100 border-double shadow-slate-300 rounded-xl shadow-sm  w-[9vw]">
+      <span className="font-bold text-white">{`INSTALADAS: ${totalPaid}`}</span>
+      <hr className="border-gray-500/30" />
+      <span className="font-bold text-white">{`INGRESADAS: ${totalSales}`}</span>
     </div>
   );
 };
