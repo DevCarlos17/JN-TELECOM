19187ea5de4923788df71b5c7e8ceb813b1024a2
Carlos <carlosmba96@gmail.com>
2023-07-04 16:06:55 -0400

    edited props in component

--- client/src/components/GraphSidebarWidget.jsx
+++ client/src/components/GraphSidebarWidget.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-const GraphSidebarWidget = ({ component, dataCard }) => {
+const GraphSidebarWidget = ({ dateFilter, userPicker, dataCard }) => {
   return (
-    <section className="flex flex-col p-2 gap-11 justify-center items-center absolute min-w-min h-min">
-      {component}
+    <section className="flex flex-col p-2 gap-11 justify-center items-center absolute w-full h-min">
+      {userPicker}
+      {dateFilter}
       {dataCard}
     </section>
   );
