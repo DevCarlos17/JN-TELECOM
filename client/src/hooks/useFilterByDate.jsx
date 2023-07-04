cdb6a0c59c2e8a30d1b169ed65c311cb0bb9ef0e
Carlos <carlosmba96@gmail.com>
2023-07-04 16:10:04 -0400

    edited filters

--- client/src/hooks/useFilterByDate.jsx
+++ client/src/hooks/useFilterByDate.jsx
@@ -12,76 +12,115 @@ import {
 const useFilterByDate = () => {
   const [filteredData, setFilteredData] = useState([]);
 
+  const fitlerByName = (data, user) => {
+    return data.filter((sale) => sale.vendedor === user.name);
+  };
+
   // Filter by week
-  const filterByWeek = (data, selectedDate) => {
+  const filterByWeek = ({ data, selectedDate, selectedUser }) => {
     const startOfWeekDate = startOfWeek(selectedDate, { weekStartsOn: 1 });
     const endOfWeekDate = endOfWeek(selectedDate, { weekStartsOn: 1 });
 
-    return data.filter((record) => {
+    const dataByWeek = data.filter((record) => {
       const itemDate = new Date(record.createdAt);
       return isWithinInterval(itemDate, {
         start: startOfWeekDate,
         end: endOfWeekDate,
       });
     });
+
+    const filteredData = selectedUser
+      ? fitlerByName(dataByWeek, selectedUser)
+      : dataByWeek;
+
+    return filteredData;
   };
 
   // Filter by month
-  const filterByMonth = (data, selectedDate) => {
+  const filterByMonth = ({ data, selectedDate, selectedUser }) => {
     const startOfMonthDate = startOfMonth(selectedDate);
     const endOfMonthDate = endOfMonth(selectedDate);
 
-    return data.filter((record) => {
+    const dataByMonth = data.filter((record) => {
       const itemDate = new Date(record.createdAt);
       return isWithinInterval(itemDate, {
         start: startOfMonthDate,
         end: endOfMonthDate,
       });
     });
+
+    const filteredData = selectedUser
+      ? fitlerByName(dataByMonth, selectedUser)
+      : dataByMonth;
+
+    return filteredData;
   };
 
   // Filter by year
-  const filterByYear = (data, selectedDate) => {
+  const filterByYear = ({ data, selectedDate, selectedUser }) => {
     const startOfYearDate = startOfYear(selectedDate);
     const endOfYearDate = endOfYear(selectedDate);
 
-    return data.filter((record) => {
+    const dataByYear = data.filter((record) => {
       const itemDate = new Date(record.createdAt);
       return isWithinInterval(itemDate, {
         start: startOfYearDate,
         end: endOfYearDate,
       });
     });
+
+    const filteredData = selectedUser
+      ? fitlerByName(dataByYear, selectedUser)
+      : dataByYear;
+
+    return filteredData;
   };
 
-  const handleFilterByWeek = (filteredRecords, selectedDate) => {
-    const filteredByWeekData = filterByWeek(filteredRecords, selectedDate);
+  const handleFilterByWeek = ({ data, selectedDate, selectedUser }) => {
+    const filteredByWeekData = filterByWeek({
+      data,
+      selectedDate,
+      selectedUser,
+    });
     setFilteredData(filteredByWeekData.reverse());
   };
 
-  const handleFilterByMonth = (filteredRecords, selectedDate) => {
-    const filteredByMonthData = filterByMonth(filteredRecords, selectedDate);
+  const handleFilterByMonth = ({ data, selectedDate, selectedUser }) => {
+    const filteredByMonthData = filterByMonth({
+      data,
+      selectedDate,
+      selectedUser,
+    });
     setFilteredData(filteredByMonthData.reverse());
   };
 
-  const handleFilterByYear = (filteredRecords, selectedDate) => {
-    const filteredByYearData = filterByYear(filteredRecords, selectedDate);
+  const handleFilterByYear = ({ data, selectedDate, selectedUser }) => {
+    const filteredByYearData = filterByYear({
+      data,
+      selectedDate,
+      selectedUser,
+    });
     setFilteredData(filteredByYearData.reverse());
   };
 
-  const filterByOption = (data, option, selectedDate) => {
-    switch (option?.code) {
+  const filterByOption = ({
+    data,
+    selectedOption,
+    selectedDate,
+    selectedUser,
+  }) => {
+    switch (selectedOption?.code) {
       case "week":
-        handleFilterByWeek(data, selectedDate);
+        handleFilterByWeek({ data, selectedDate, selectedUser });
         break;
 
       case "month":
-        handleFilterByMonth(data, selectedDate);
+        handleFilterByMonth({ data, selectedDate, selectedUser });
 
         break;
 
       case "year":
-        handleFilterByYear(data, selectedDate);
+        handleFilterByYear({ data, selectedDate, selectedUser });
 
         break;
 
