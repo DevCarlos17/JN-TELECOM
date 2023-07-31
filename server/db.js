d066373bc2c79dcfcbd2df824ebdc0399ffd91d6
Carlos <dev.carlos17@gmail.com>
2023-07-31 10:26:20 -0400

    unused lines removed

--- server/db.js
+++ server/db.js
@@ -1,8 +1,6 @@
 import mongoose from "mongoose";
 import { MONGODB_URI } from "./config.js";
 
-
-
 export async function connectDB() {
   try {
     const db = await mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, autoIndex: false });
