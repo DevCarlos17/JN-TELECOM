0127636f4766290e104ca6c344420453e2f68307
Carlos <dev.carlos17@gmail.com>
2023-12-26 19:03:50 -0400

    edited services name

--- client/src/helper/streamingServices.js
+++ client/src/helper/streamingServices.js
@@ -1,12 +1,12 @@
 export const STREAMING_SERVICES = [
   { id: 1, name: "NETFLIX" },
   { id: 3, name: "HBO MAX" },
-  { id: 2, name: "DISNEY+" },
-  { id: 3, name: "PRIME VIDEO" },
-  { id: 3, name: "CRUNCHYROLL" },
-  { id: 3, name: "PARAMOUNT" },
-  { id: 3, name: "STAR+" },
-  { id: 3, name: "IPTV - INCLUYE LA LIGA 1 MAX Y GOL PERÙ" },
+  { id: 4, name: "DISNEY+" },
+  { id: 5, name: "PRIME VIDEO" },
+  { id: 6, name: "CRUNCHYROLL" },
+  { id: 7, name: "PARAMOUNT+" },
+  { id: 8, name: "STAR+" },
+  { id: 9, name: "IPTV" },
 ];
 
 export const RENOVATION_TYPES = ["VIGENTE", "GRATIS", "VENCIDO"]
