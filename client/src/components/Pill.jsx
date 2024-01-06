a32e22041468ac225a4ccc3a6f1efe419d84e774
Carlos <dev.carlos17@gmail.com>
2024-01-06 11:21:31 -0400

    add pill

---
+++ client/src/components/Pill.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Pill = ({ name, value }) => {
+  const getSeverity = (status) => {
+    switch (status) {
+      case "VIGENTE":
+        return "lime-500";
+
+      case "GRATIS":
+        return "sky-500";
+
+      case "VENCIDO":
+        return "red-500";
+    }
+  };
+
+  const color = getSeverity(name);
+
+  return (
+    <div
+      className={`bg-${color}/10 flex justify-center items-center py-1.5 px-4 border border-${color}/20 rounded-full gap-x-2 transition hover:bg-${color}/20`}>
+      <span className="text-base">
+        {name}: {value}
+      </span>
+    </div>
+  );
+};
+
+export default Pill;
